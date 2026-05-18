00d9e35d1e30f90102ab0721caa112bb9553ae54
KDF25 <karimov.damir.faridovich@gmail.com>
2026-05-19 04:02:34 +0500

    feat: add GA4

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,6 +7,7 @@ import {
 	setRequestLocale
 } from "next-intl/server";
 import { notFound } from "next/navigation";
+import Script from "next/script";
 
 import {
 	DocHeader,
@@ -93,6 +94,27 @@ export default async function LocaleLayout({ children, params }: Props) {
 			<body
 				className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
 			>
+				{/* Google Tag Manager */}
+				<Script id="gtm" strategy="afterInteractive">
+					{`
+						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+						f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-P5PZXHFR');
+					`}
+				</Script>
+
+				{/* Google Tag Manager (noscript) */}
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-P5PZXHFR"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					/>
+				</noscript>
 				<NextIntlClientProvider messages={messages} locale={locale}>
 					<ThemeProvider>
 						<SidebarProvider>
